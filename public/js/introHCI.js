70a7b30c4fd230d356672edb435dd7c654f910f4
Young Jin Yun <yngjnyn@gmail.com>
2016-02-11 13:17:23 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,11 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$(".project").click(function(e){
+		console.log("/project/" + idNumber);
+		$.get("/project/" + idNumber, addProject);
+	})
 }
 
 /*
@@ -34,5 +39,24 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
+	$.get("palette",colorDisco)
 	console.log("User clicked on color button");
+}
+
+function addProject(result) {
+  var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="img">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p></a>'; 
+}
+
+function colorDisco(result){
+	var colors = result.colors.hex;
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
